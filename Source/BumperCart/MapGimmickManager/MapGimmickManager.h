de69c2b73ca5eef6efdb17f932798c87896330f4
NaReun73 <rnlcksg37@gmail.com>
2026-07-02 11:15:33 +0900

    feat: 카트 돌진 기믹 추가 및 코드 정리

--- Source/BumperCart/MapGimmickManager/MapGimmickManager.h
+++ Source/BumperCart/MapGimmickManager/MapGimmickManager.h
@@ -6,6 +6,8 @@
 
 class AWaterHoleGimmick;
 class AObstacleGimmick;
+class UNiagaraSystem;
+class ANPCRushGimmick;
 
 USTRUCT(BlueprintType)
 struct FObstacleSpawnInfo
@@ -43,6 +45,9 @@ private:
     UPROPERTY()
     TArray<TObjectPtr<class ATargetPoint>> GimmickSpawnPointList;
 
+    UPROPERTY()
+    TArray<TObjectPtr<class ATargetPoint>> NPCRushStartPointList;
+
 #pragma endregion
 
 #pragma region Gimmick
@@ -55,6 +60,13 @@ private:
     TArray<FObstacleSpawnInfo> ObstacleSpawnList;
 
     TArray<AActor*> SpawnedObstacleList;
+
+    FTimerHandle RespawnTimerHandle;
+
+protected:
+    // 장애물 리스폰 시간
+    UPROPERTY(EditAnywhere, Category = "Gimmick | WaterHole")
+    float ObstacleRespawnInterval = 20.0f;
 
 public:
     // 게임 시작시 호출 - 게임 모드에서 호출
@@ -74,53 +86,15 @@ public:
 
 #pragma endregion
 
-
-#pragma region Water Hole
-protected:
-    // 스폰할 물 웅덩이
-    UPROPERTY(EditAnywhere, Category = "Gimmick | WaterHole")
-    TSubclassOf<AWaterHoleGimmick> WaterHoleClass;
-
-    // 스폰될 물 웅덩이 수
-    UPROPERTY(EditAnywhere, Category = "Gimmick | WaterHole")
-    int32 TotalWaterHoleSpawnCount = 3;
-
-    // 물 웅덩이 리스폰 시간
-    UPROPERTY(EditAnywhere, Category = "Gimmick | WaterHole")
-    float WaterHoleRespawnInterval = 20.0f;
-
+#pragma region NPC Rush
 private:
-    // 스폰된 물 웅덩이 저장
-    UPROPERTY()
-    TArray<TObjectPtr<class AWaterHoleGimmick>> SpawnedWaterHoles;
+    UPROPERTY(EditAnywhere, Category = "Gimmick | NPC Rush")
+    TSubclassOf<ANPCRushGimmick> NPCRushGimmick;
 
-    FTimerHandle RespawnTimerHandle;
-
-    // 물 웅덩이 리스폰
-    void RespawnWaterHole();
-
-    // 스폰된 물 웅덩이 정리
-    void ClearAllWaterHole();
-
-    // 물 웅덩이 스폰
-    void SpawnWaterHole();
-
-    // 라운드 끝 정리용
-    void EndSpawnWaterHole();
+public:
+    void StartNPCRush();
 
 #pragma endregion
-
-#pragma region Obstacle
-protected:
-    // 스폰할 장애물
-    UPROPERTY(EditAnywhere, Category = "Gimmick | Obstacle")
-    TArray<TSubclassOf<AObstacleGimmick>> ObstacleList;
-
-    UPROPERTY(EditAnywhere, Category = "Gimmick | Obstacle")
-    int32 TotalObstacleSpawnCount = 5;
-
-#pragma endregion
-
 
 
 };
---
+++ Source/BumperCart/MapGimmickManager/NPCRushGimmick/NPCRushGimmick.cpp
@@ -0,0 +1,58 @@
+﻿#include "MapGimmickManager/NPCRushGimmick/NPCRushGimmick.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+#include "Kismet/KismetStringLibrary.h"
+#include "Cart/CartPawn.h"
+
+ANPCRushGimmick::ANPCRushGimmick()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+
+    bReplicates = true;
+
+    BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+    SetRootComponent(BoxCollision);
+
+    CartMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CartMesh"));
+    CartMesh->SetupAttachment(RootComponent);
+    CartMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    NPCRushFX = CreateDefaultSubobject<UNiagaraComponent>(TEXT("NiagaraComponent"));
+    NPCRushFX->SetupAttachment(CartMesh);
+
+    ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+
+    if (IsValid(ProjectileMovement))
+    {
+        ProjectileMovement->InitialSpeed =1000.0f;
+        ProjectileMovement->MaxSpeed = 1000.0f;
+        ProjectileMovement->ProjectileGravityScale = 0.0f;
+    }
+    
+}
+
+void ANPCRushGimmick::BeginPlay()
+{
+	Super::BeginPlay();
+
+    if (HasAuthority() && BoxCollision)
+    {
+        BoxCollision->OnComponentBeginOverlap.AddDynamic(this, &ANPCRushGimmick::OnCartOverlap);
+    }
+}
+
+void ANPCRushGimmick::OnCartOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (!OtherActor || OtherActor == this) return;
+
+    if (OtherComp && OtherComp->GetCollisionObjectType() == ECC_WorldStatic)
+    {
+        UE_LOG(LogTemp, Log, TEXT("[맵 기믹 매니저] 거대 카트가 마트 벽에 부딪혀 박살났습니다!"));
+
+        Destroy();
+    }
+}
+
