325acb119860e276d7ee06048ab070b7808bb3a3
zxcv11595 <zxcv11595@gmail.com>
2026-07-04 01:24:54 +0900

    feat: 인벤토리 컴포넌트 구현
    - 아이템 하나만 보유하는 단일 슬롯
    - 보유 아이템 서버에서 관리
    - ItemData 복제, RepNotify 적용
    - 클라이언트 아이템 사용을 서버 PRC로 전달
    - 아이템 Action 실행 성공 시 아이템 소모

--- Source/BumperCart/Cart/Component/CartItemInventoryComponent.h
+++ Source/BumperCart/Cart/Component/CartItemInventoryComponent.h
@@ -4,6 +4,8 @@
 #include "Components/ActorComponent.h"
 #include "CartItemInventoryComponent.generated.h"
 
+class ACartPawn;
+class UItemDataAsset;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUMPERCART_API UCartItemInventoryComponent : public UActorComponent
@@ -13,5 +15,65 @@ class BUMPERCART_API UCartItemInventoryComponent : public UActorComponent
 public:	
 	UCartItemInventoryComponent();
 
-		
+protected:
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+    
+// ------------------------------------------------------------
+// 아이템 정보
+// ------------------------------------------------------------
+private:
+    // 현재 보유 중인 아이템
+    UPROPERTY(ReplicatedUsing = OnRep_CurrentItemData)
+    TObjectPtr<UItemDataAsset> CurrentItemData;
+
+// ------------------------------------------------------------
+// 아이템 획득
+// ------------------------------------------------------------
+public:
+    // 서버에서 아이템 획득
+    // 기존 아이템이 있을 경우 새 아이템으로 교체
+    bool AcquireItem(UItemDataAsset* NewItemData);
+
+// ------------------------------------------------------------
+// 아이템 사용
+// ------------------------------------------------------------
+public:
+    // 로컬 플레이어가 아이템 사용 요청
+    void RequestUseItem();
+
+private:
+    // 서버 RPC
+    // 클라이언틔의 아이템 사용을 서버로 전달
+    UFUNCTION(Server, Reliable)
+    void ServerUseItem();
+
+    // 서버에서 아이템의 Action을 실행
+    void UseItem();
+
+    // 서버에서 현재 보유 아이템 제거
+    void ClearItem();
+
+// ------------------------------------------------------------
+// Getter
+// ------------------------------------------------------------
+public:
+    // 아이템 보유 여부
+    UFUNCTION(BlueprintPure, Category = "Item")
+    bool HasItem() const;
+
+    // 현재 아이템 데이터 반환
+    UFUNCTION(BlueprintPure, Category = "Item")
+    UItemDataAsset* GetCurrentItemData() const;
+
+// ------------------------------------------------------------
+// 아이템 변경 
+// ------------------------------------------------------------
+private:
+    // 아이템 변경 후 처리
+    void HandleItemChanged();
+
+    // 아이템 복제 시 호출
+    UFUNCTION()
+    void OnRep_CurrentItemData();
 };
