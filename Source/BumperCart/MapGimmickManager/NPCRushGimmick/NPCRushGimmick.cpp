eb381dcfa54e12792f1a8d654f0b4cd97cfa5106
NaReun73 <rnlcksg37@gmail.com>
2026-07-13 15:57:40 +0900

    feat: NPC 러시·워터홀 사운드

--- Source/BumperCart/MapGimmickManager/NPCRushGimmick/NPCRushGimmick.cpp
+++ Source/BumperCart/MapGimmickManager/NPCRushGimmick/NPCRushGimmick.cpp
@@ -8,6 +8,8 @@
 #include "Cart/CartPawn.h"
 #include "Cart/CartLoadTypes.h"
 #include "Cart/Component/CartLoadComponent.h"
+#include "Components/AudioComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 ANPCRushGimmick::ANPCRushGimmick()
 {
@@ -27,6 +29,9 @@ ANPCRushGimmick::ANPCRushGimmick()
 
     ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
 
+    NPCRushAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("NPCRushAudio"));
+    NPCRushAudioComponent->SetupAttachment(RootComponent);
+
     if (IsValid(ProjectileMovement))
     {
         ProjectileMovement->InitialSpeed =1000.0f;
@@ -34,11 +39,22 @@ ANPCRushGimmick::ANPCRushGimmick()
         ProjectileMovement->ProjectileGravityScale = 0.0f;
     }
 
+    NPCRushAudioComponent->bAutoActivate = true;
 }
 
 void ANPCRushGimmick::BeginPlay()
 {
 	Super::BeginPlay();
+
+    if (SpawnSound)
+    {
+        UGameplayStatics::PlaySoundAtLocation(GetWorld(), SpawnSound, GetActorLocation());
+    }
+
+    if (NPCRushAudioComponent && NPCRushAudioComponent->IsActive())
+    {
+
+    }
 
     if (HasAuthority() && BoxCollision)
     {
